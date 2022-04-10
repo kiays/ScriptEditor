f30446abe85c2c6c8f81bae607afe06431e99baf
kiays <1128130+kiays@users.noreply.github.com>
2022-04-10 22:15:43 +0900

    -

--- renderer/components/CsvEditor.tsx/Editor.tsx
+++ renderer/components/CsvEditor.tsx/Editor.tsx
@@ -1,24 +1,81 @@
-import React, { FC, MutableRefObject, useEffect, useRef } from "react";
+import React, { FC, useState } from "react";
+import { Table, TableBody, TableCell, TableHead, TableRow, TextField } from "@mui/material";
 import WaveForm from "./WaveForm";
+import { Redo as ClockwiseIcon, Undo as AnticlockwiseIcon } from "@mui/icons-material"
 
 type Props = {
 
 };
 
+const csv = [
+  [0, 0, 0, 0, 0],
+  [100, 1, 30, 1, 40],
+  [200, 0, 30, 0, 40],
+  [300, 1, 30, 1, 40],
+  [400, 0, 30, 0, 40],
+  [500, 1, 30, 1, 40],
+  [600, 0, 30, 0, 40],
+  [700, 1, 30, 1, 40],
+  [800, 0, 30, 0, 40],
+]
+
+const EDITABLES = {
+  RIGHT_DIR: "RIGHT_DIR",
+  RIGHT_VAL: "RIGHT_VAL",
+  LEFT_DIR: "LEFT_DIR",
+  LEFT_VAL: "LEFT_VAL",
+}
+
+const DirCell = ({ value, target, currentTarget, onChange, setTarget }) => {
+  return (<TableCell onClick={(e) => { e.stopPropagation(); setTarget(target) }}>
+    {currentTarget !== target ?
+      (value === 0 ? "left" : "right") :
+      (<select defaultValue={value} onChange={(e) => console.log(e.target.value)}>
+        <option value={0}>left : 0</option>
+        <option value={1}>right : 1</option>
+      </select>)}</TableCell>)
+};
+
+const NumCell = ({ value, target, currentTarget, setTarget }) => {
+  return (<TableCell onClick={(e) => { e.stopPropagation(); setTarget(target) }}
+  >
+
+    {currentTarget !== target ?
+      (value) : <TextField type={"number"} />
+    }
+  </TableCell>)
+}
+const TimeRow = ({ data: [time, leftDir, leftVal, rightDir, rightVal] }) => {
+  const [target, setTarget] = useState(null);
+  console.log(target)
+  return (<TableRow key={time + "right"} onClick={() => setTarget(null)}>
+    <TableCell>{time}</TableCell>
+    <DirCell value={leftDir} target={EDITABLES.LEFT_DIR} currentTarget={target} setTarget={setTarget} />
+    <TableCell>{leftVal}</TableCell>
+    <DirCell value={rightDir} target={EDITABLES.RIGHT_DIR} currentTarget={target} setTarget={setTarget} />
+    <TableCell>{rightVal}</TableCell>
+  </TableRow>)
+}
+
 const CsvEditor: FC<Props> = ({ }) => {
-    const canvas: MutableRefObject<HTMLCanvasElement> = useRef(null);
+  const [timesheet, setTimesheet] = useState<Array<[number, number, number, number, number]>>(csv);
 
-    useEffect(() => {
-        if (!canvas || !canvas.current) return;
-        const cvs = canvas.current;
-        const ctx = cvs.getContext("2d");
-        ctx.fillRect(0,0,50,100);
-    })
-
-    return (<div>
-        <canvas ref={canvas}></canvas>
-        <WaveForm />
-    </div>)
+  return (<div>
+    <Table >
+      <TableHead>
+        <TableRow>
+          <TableCell>time (x10msec)</TableCell>
+          <TableCell>left dir</TableCell>
+          <TableCell>left value</TableCell>
+          <TableCell>right dir</TableCell>
+          <TableCell>right value</TableCell>
+        </TableRow>
+      </TableHead>
+      <TableBody>
+        {timesheet.map((data) => (<TimeRow key={data[0]} data={data} />))}
+      </TableBody></Table>
+    <WaveForm />
+  </div>)
 }
 
 export default CsvEditor;
---
+++ renderer/components/CsvEditor.tsx/GraphicalEditor.tsx
@@ -0,0 +1,41 @@
+import React, { FC } from "react";
+
+type Props = {
+
+};
+
+const csv = [
+  [0, 0, 0, 0, 0],
+  [100, 1, 30, 1, 40],
+  [200, 0, 30, 0, 40],
+  [300, 1, 30, 1, 40],
+  [400, 0, 30, 0, 40],
+  [500, 1, 30, 1, 40],
+  [600, 0, 30, 0, 40],
+  [700, 1, 30, 1, 40],
+  [800, 0, 30, 0, 40],
+]
+
+const GraphicalEditor: FC<Props> = ({ }) => {
+  return (<div>
+    <svg
+      viewBox="0 0 800 100"
+      width={"100%"}
+      height={"300px"}
+      xmlns="<http://www.w3.org/2000/svg>"
+      style={{ border: "1px solid red" }}
+    >
+      <circle
+        cx="12" cy="12" r="8"
+        strokeWidth="1" stroke="tomato"
+        fill="none"
+      />
+      {csv.map(([duration, leftDir, leftVal, rightDir, rightVal]) => (
+        <rect onClick={console.log} x={duration} y={leftVal} width={50} height={10} fill={leftDir == 0 ? "red" : "blue"} />
+      ))}
+
+    </svg>
+  </div>)
+}
+
+export default GraphicalEditor;
